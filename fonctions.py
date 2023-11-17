684056137bd60e6cc2e1f97c02423db14649a214
Shiteur <72656921+Shiteur@users.noreply.github.com>
2023-11-17 13:37:52 +0100

    ajout de fonction

--- fonctions.py
+++ fonctions.py
@@ -10,6 +10,12 @@ def nom_président(files_names):
     president = []
     for  i in range(len(files_names)):
         president.append(files_names[i][11:])
-        while 49<=ord(president[i][-1])<=57:
+        while 49<=ord(president[i][-1])<=57: # permet d'enlevé les chiffres à la fin des noms de présidents
             president[i]= president[i][:-1]
     return president
+
+def prénom_président(nom):
+    prenom={"Chirac":"Jacques","Giscard dEstaing":"Valéry","Mitterrand":"François","Sarkozy":"Nicolas","Macron":"Emmanuel","Hollande":"François"}
+    for cle, valeur in prenom.items():
+        if nom ==cle:
+            return prenom[nom]
