0f8f9e93624211b0948e9d89371578aef64dfd2c
Shiteur <72656921+Shiteur@users.noreply.github.com>
2023-11-17 11:38:45 +0100

    fiojion

--- python_files/main.py
+++ python_files/main.py
@@ -1,5 +1,5 @@
 from fonctions import *
 # appelle la fonction list_of_files
-directory = "./speeches"
+directory = "../speeches"
 files_names = list_of_files(directory, "txt")
 print_list(files_names)
