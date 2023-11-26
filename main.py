41b0fdfa90263c61fe58b5a14e62c7c09d8dac67
rayfox99 <rayan.eid@outlook.fr>
2023-11-26 23:32:36 +0100

    Dernier changement partie 1

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ surpession_ponctuation()
 
 def menu():
     print("Menu:")
-    print("1. Afficher la liste des mots les moins importants")
-    print("2. Afficher le mot avec le score TD-IDF le plus élevé")
-    print("3. Indiquer le(s) mot(s) le(s) plus répété(s) par le président Chirac")
-    print("4. Indiquer le(s) nom(s) du (des) président(s) qui a (ont) parlé de la « Nation »")
-    print("5. Indiquer le premier président à parler du climat et/ou de l'écologie")
+    print("1. Affiche la liste des mots les moins importants")
+    print("2. Affiche le mot avec le score TD-IDF le plus élevé")
+    print("3. Indique le(s) mot(s) le(s) plus répété(s) par le président Chirac")
+    print("4. Indique le(s) nom(s) du (des) président(s) qui a (ont) parlé de la « Nation »")
+    print("5. Indique le premier président à parler du climat et/ou de l'écologie")
     print("6. Mot(s) évoqués par tous les présidents (hormis les mots non importants)")
     print("7. Pour quitter.")
 start=True
