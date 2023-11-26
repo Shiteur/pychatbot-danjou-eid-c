ad1abc4253774eddba3978349d12b3deb141feab
Shiteur <72656921+Shiteur@users.noreply.github.com>
2023-11-26 19:52:44 +0100

    ajout de fonctions

--- fonctions.py
+++ fonctions.py
@@ -28,7 +28,7 @@ def retourne_nom_président(liste_nom):
             del(liste_nom[i])
         else:
             i+=1
-    print(liste_nom)
+    return liste_nom
 
 def convertion_minuscule():
     list_text = list_of_files("speeches", "txt")#récuprère la liste des fichier .txt du répertoir speeches
@@ -86,14 +86,17 @@ def IDF(cleaned):
     for i in range(len(files)):
         with open(cleaned+"/"+files[i], "r") as f1:
             line = f1.readline()
-            while line != "":
-                
+            mot=[]
+            while line != "":               
                 k = TF(line)
                 for keys in k.keys():
-                    if keys in IDF_score.keys() :
-                        IDF_score[keys] += k[keys]
+                    if not(keys in IDF_score.keys()) :
+                        IDF_score[keys] = 1
+                        mot.append(keys)
                     else :
-                        IDF_score[keys] = k[keys]
+                        if not(keys in mot):
+                            IDF_score[keys] += 1
+                            mot.append(keys)
                 line = f1.readline()        
     for keys in IDF_score.keys() :
         IDF_score[keys] = math.log(len(files)/IDF_score[keys]) 
@@ -119,4 +122,80 @@ def TF_IDF(repertoire):
         matrice.append(TF_score)
         list_mot.append(key)
     return (list_mot, matrice)
-print(TF_IDF("cleaned"))
+
+def mot_pas_important():
+    tf_idf = TF_IDF("cleaned")
+    mot_non_impotant=[]
+    for i in range(len(tf_idf[1])):
+        s=0
+        for j in range(len(tf_idf[1][i])):
+            if tf_idf[1][i][j]==0.0:
+                s+=1
+        if s==len(tf_idf[1][i]):
+            mot_non_impotant.append(tf_idf[0][i])
+    return mot_non_impotant
+
+def mot_important():
+    tf_idf = TF_IDF("cleaned")
+    mot_impotant=[]
+    val=[]
+    M=0
+    for i in range(len(tf_idf[1])):
+        s=0
+        for j in range(len(tf_idf[1][i])):
+            s+=tf_idf[1][i][j]
+        if s>M:                             #recherche de maximum
+           M=s
+        val.append([s,i])
+    for i in range(len(val)):
+       if val[i][0]==M:
+            mot_impotant.append(tf_idf[0][val[i][1]])
+    return mot_impotant
+
+def mot_plus_repeter_par_chirac():
+    TF_score = {}
+    mot_plus_dit= []
+    files = list_of_files("cleaned", "txt")
+    for i in range(0,2):                            #calcule le nombre de fois que chriac à prononcé un mot
+        with open("cleaned/"+files[i], "r") as f1:
+            line = f1.readline()
+            while line != "":               
+                k = TF(line)
+                for keys in k.keys():
+                    if keys in TF_score.keys() :
+                        TF_score[keys] += k[keys]
+                    else :
+                        TF_score[keys] = k[keys]
+                line = f1.readline() 
+    M=0
+    for key,values in TF_score.items() :            #recherche le maximum de fois que chirac à proncé un même mot
+        if values>M:
+            mot_plus_dit= []
+            mot_plus_dit.append(key)
+        else: 
+            if values==M:
+                mot_plus_dit.append(key)
+    return mot_plus_dit
+
+def nation():
+    president = list_of_files("cleaned", "txt")
+    president = nom_président(president)
+    tf_idf = TF_IDF("cleaned")
+    n=0
+    i=0
+    while i <len(tf_idf[0]) and tf_idf[0][i]!="nation":
+        i+=1
+    n=i
+    m=1
+    i=0
+    while i<len(tf_idf[1][n]):
+        if tf_idf[1][n][i]!=0.0:
+            if m>tf_idf[1][n][i]:
+                m=tf_idf[1][n][i]
+                nom=president[i]
+            i+=1
+        else:
+            del(president[i])
+            del(tf_idf[1][n][i])
+    president=retourne_nom_président(president)
+    print(f"le nom des présidents qui ont le parler de la nation sont {president} et celui qui en à le plus parler est {nom}")
